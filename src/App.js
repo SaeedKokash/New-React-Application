3c0e283f48e16578ca7970b79344afebe5b5562a
saeedkokash <saeedkokash@gmail.com>
2022-07-24 17:56:23 +0300

    added a header-main-footer js with task1 requirements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Header/>
       </header>
+
+      <main className="App-main">
+          <Main className="mainItem" title="UniWhal" imgLink="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" imgAlt="UniWhal" imgtitle="UniWhal"
+          description="A unicorn and a narwhal nuzzling their horns"/>
+
+          <Main title="Rhino Family" imgLink="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" imgAlt="Rhino Family" imgtitle="Rhino Family"
+          description="Parent rhino with two babies"/>
+
+          <Main title="Unicorn Head" imgLink="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" imgAlt="Unicorn Head" imgtitle="Unicorn Head"
+          description="Someone wearing a very silly unicorn head mask"/>
+      </main>
+
+      <footer className="App-footer">
+          <Footer/>
+      </footer>
     </div>
   );
 }
