b0c7fed3e6efe3deff365f14bd0dac79748b29c8
saeedkokash <saeedkokash@gmail.com>
2022-07-24 18:48:53 +0300

    added the hornedbeast component which I missed in the last PR

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,20 @@
 import React from "react";
+import HornedBeast from "./HornedBeast";
 
 class Main extends React.Component {
     render () {
         return (
             <div>
 
-                <h2 className="animalTitle">{this.props.title}</h2>
+                <HornedBeast className="mainItem" title="UniWhal" imgLink="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" imgAlt="UniWhal" imgtitle="UniWhal"
+                description="A unicorn and a narwhal nuzzling their horns"/>
 
-                {/* <img className="animalImg" src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt={"UniWhal"} title="UniWhal"/> */}
+                <HornedBeast className="mainItem" title="Rhino Family" imgLink="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" imgAlt="Rhino Family" imgtitle="Rhino Family"
+                description="Parent rhino with two babies"/>
 
-                <img className="animalImg" src={this.props.imgLink} alt={this.props.imgAlt} title={this.props.imgTitle}/>
-    
-                <p> {this.props.description}</p>
-
+                <HornedBeast className="mainItem" title="Unicorn Head" imgLink="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" imgAlt="Unicorn Head" imgtitle="Unicorn Head"
+                 description="Someone wearing a very silly unicorn head mask"/>
+            
             </div>
 
         )
